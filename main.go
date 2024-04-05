b1db2db2324b498c07cd323c4ecbe24cbb5562b0
Adam Hutton <adam1.hutton1@gmail.com>
2024-04-05 15:22:44 +0100

    working refresh and switching of displayed IP

--- main.go
+++ main.go
@@ -12,20 +12,36 @@ import (
 	"strings"
 )
 
-var addresses []string
+var Addresses []string
+var ipIcons []*systray.Systray
+var ipMenuItems []*glSystray.MenuItem
 
 func main() {
-	addresses = getAddressesRegex()
+	Addresses = getAddressesRegex()
+	err := clipboard.Init()
+	if err != nil {
+		println("Error initialising clipboard")
+	}
+	glSystray.Run(func() {
+		createControlIcon()
+		createIpTrayIcons(Addresses, 0)
+	}, func() {
+		println("On Exit")
+	})
+
+}
+
+func createIpTrayIcons(addresses []string, index int) {
 	allAddressesHint := strings.Join(addresses, "\n")
-	address := addresses[0]
+	address := addresses[index]
 	numbers := strings.Split(address, ".")
 	slices.Reverse(numbers)
-
-	err := clipboard.Init()
+	ipIcons = make([]*systray.Systray, 0)
 
 	var tray *systray.Systray
 	for i, number := range numbers {
 		tray = systray.New("", "localhost", 6320+i)
+		ipIcons = append(ipIcons, tray)
 		tray.OnClick(func() {
 			clipboard.Write(clipboard.FmtText, []byte(address))
 		})
@@ -36,36 +52,62 @@ func main() {
 	}
 
 	go func() {
-		if err = tray.Run(); err != nil {
+		if err := tray.Run(); err != nil {
 			println(err.Error())
 		}
 	}()
-	glSystray.Run(glOnReady, func() {})
-
 }
 
-func glOnReady() {
+func removeIpIcons() {
+	for _, trayIcon := range ipIcons {
+		if err := trayIcon.Stop(); err != nil {
+			println(err.Error())
+		}
+	}
+}
+
+func createControlIcon() {
 	glSystray.SetIcon(icon.Data)
 	glSystray.SetTitle("Refresh Address")
 	glSystray.SetTooltip("Do refreshing")
 	refreshBtn := glSystray.AddMenuItem("Refresh", "Reload to refresh IPs")
+	addIpIcons()
 	go func() {
 		for {
 			<-refreshBtn.ClickedCh
 			println("Refresh clicked")
+			Addresses = getAddressesRegex()
+			removeIpIcons()
+			removeControlIpsList()
+			addIpIcons()
+			createIpTrayIcons(Addresses, 0)
 		}
 	}()
-	for _, address := range addresses {
+
+}
+
+func addIpIcons() {
+	ipMenuItems = make([]*glSystray.MenuItem, 0)
+	for i, address := range Addresses {
 		btn := glSystray.AddMenuItem(fmt.Sprintf("Change to %s", address), "")
-		go func(button *glSystray.MenuItem, address string) {
+		ipMenuItems = append(ipMenuItems, btn)
+		go func(button *glSystray.MenuItem, index int) {
 			for {
 				<-button.ClickedCh
-				fmt.Printf("swtiching to %s\n", address)
+				fmt.Printf("swtiching to %d\n", index)
+				removeIpIcons()
+				createIpTrayIcons(Addresses, index)
 			}
 
-		}(btn, address)
+		}(btn, i)
 	}
+}
 
+func removeControlIpsList() {
+	for _, item := range ipMenuItems {
+		item.Disable()
+		item.Hide()
+	}
 }
 
 func getAddressesRegex() []string {
